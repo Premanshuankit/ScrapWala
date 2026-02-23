170095cf51302cf8cb2c46f7ef567bd33246e715
Premanshuankit <ankit.premanshu@gmail.com>
2026-02-23 13:55:13 +0530

    setting new relic log for homeRoutes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,9 @@
+const logger = require('../utils/logger')
 
-const handleHome = (req, res) => {
-    console.log('home triggered')
+const handleHome = async (req, res) => {
+    logger.info(`HOME HIT ${Date.now()}`)
+    console.log(`HOME HIT ${Date.now()}`)
+    await new Promise(resolve => setTimeout(resolve, 500))
     res.json({ message: "Backend working" })
 }
 
