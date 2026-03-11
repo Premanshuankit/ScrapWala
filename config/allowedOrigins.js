7fe767347ef4c4af62c5974bdc6c152369fd4d5f
Premanshuankit <ankit.premanshu@gmail.com>
2026-03-11 13:02:00 +0530

    added new allowedorigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,6 @@
 const allowedOrigins = [
-    'https://scrap-wala-frontend-hzhx.vercel.app',
-    'https://scrapwallah.onrender.com',
+    'https://scrap-wala-frontend.vercel.app',
     'http://127.0.0.1:3000',
-    'http://localhost:3500',
     'http://localhost:5173',
 ]
 
