bb8021e822be320803cc5ccf9511e8420a926480
Premanshuankit <ankit.premanshu@gmail.com>
2026-03-13 19:12:32 +0530

    final origin added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://scrap-wala-frontend.vercel.app',
+    'https://scrap-management-app.vercel.app',
     'http://127.0.0.1:3000',
     'http://localhost:5173',
 ]
